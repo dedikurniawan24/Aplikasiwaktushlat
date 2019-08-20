519b4d53f79a9a071129b5c4a3231c6aaffce733
dedikurniawan24 <dedi16259@gmail.com>
2019-08-20 13:13:25 +0700

    Initial commit

---
+++ app/src/main/java/com/dedi/aplikasitimeshalat/Kota.kt
@@ -0,0 +1,12 @@
+package com.dedi.aplikasitimeshalat
+
+class Kota {
+
+
+
+    var id: Int? = null
+    var nama: String? = null
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
